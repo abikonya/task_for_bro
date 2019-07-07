1c53dd7962e34870ac594516dd2b6983e292f2ad
abikonya <36229338+abikonya@users.noreply.github.com>
2019-07-07 15:33:52 +0300

    add pagination, settings, login/logout

--- gallery_site/gunicorn.conf.py
+++ gallery_site/gunicorn.conf.py
@@ -1,4 +1,4 @@
-command = '/root/task_for_bro/venv/bin/gunicorn'
-pythonpath = '/root/task_for_bro'
+command = '/home/task_for_bro/venv/bin/gunicorn'
+pythonpath = '/home/task_for_bro'
 bind = '127.0.0.1:80'
 workers = 3
